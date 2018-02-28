0118c287526da88279a329005de33b42ff87453e
Joe Ortega <joeortega79@gmail.com>
2018-02-28 22:24:47 +1030

    downloads and displays a celebritys image

--- app/src/main/java/com/udemy/sbsapps/guessthecelebrity/MainActivity.java
+++ app/src/main/java/com/udemy/sbsapps/guessthecelebrity/MainActivity.java
@@ -1,13 +1,127 @@
 package com.udemy.sbsapps.guessthecelebrity;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    DownloadTask task;
+
+    ArrayList<String> celebURLs = new ArrayList<>();
+    ArrayList<String> celebNames = new ArrayList<>();
+
+    int chosenCeleb = 0;
+
+    ImageView imageView;
+
+    public class ImageDownLoader extends  AsyncTask<String, Void, Bitmap>{
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            try {
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+
+                Bitmap myBitmaop = BitmapFactory.decodeStream(inputStream);
+
+                return myBitmaop;
+
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String>{
+        @Override
+        protected String doInBackground(String... urls) {
+
+            String result = "";
+            URL url;
+            HttpURLConnection urlConnection = null;
+
+            try{
+                url = new URL(urls[0]);
+
+                urlConnection = (HttpURLConnection) url.openConnection();
+
+                InputStream in = urlConnection.getInputStream();
+
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while(data != -1){
+                    char current = (char) data;
+                    result+= current;
+                    data = reader.read();
+                }
+
+                return result;
+            } catch (Exception e){
+                e.printStackTrace();
+                return null;
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView = findViewById(R.id.imageView);
+
+        task = new DownloadTask();
+        String result = null;
+
+        try {
+            result = task.execute("http://www.posh24.se/kandisar").get();
+
+            String[] splitResult = result.split("<div class=\"listedArticles\">");
+
+            Pattern p = Pattern.compile("img src=\"(.*?)\"");
+            Matcher m = p.matcher(splitResult[0]);
+
+            while (m.find()){
+                celebURLs.add(m.group(1));
+            }
+
+            p = Pattern.compile("alt=\"(.*?)\"");
+            m = p.matcher(splitResult[0]);
+
+            while (m.find()){
+                celebNames.add(m.group(1));
+            }
+            Random rand = new Random();
+            chosenCeleb = rand.nextInt(celebNames.size());
+
+            ImageDownLoader imageTask = new ImageDownLoader();
+
+            Bitmap celebImage = imageTask.execute(celebURLs.get(chosenCeleb)).get();
+
+            imageView.setImageBitmap(celebImage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
